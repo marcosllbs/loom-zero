bb03f0e08afa2f0d55da498d68169e04500aa9e3
Marcos Lucas <42158901+marcosllbs@users.noreply.github.com>
2025-11-17 18:44:31 -0300

    Script Card Creation

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Card : MonoBehaviour
+{
+
+  [SerializeField] Image frontImage;
+  [SerializeField] Image backImage;
+  [SerializeField] Button cardButtom;
+
+}
