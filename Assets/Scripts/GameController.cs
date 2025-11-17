9d3bed024fe0e2f10604c5a09eb35af258206786
Marcos Lucas <42158901+marcosllbs@users.noreply.github.com>
2025-11-17 20:36:23 -0300

    Add GameController References

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    [Header("Board Config")]
+    [SerializeField] int rows;
+    [SerializeField] int columns;
+
+    [Header("References")]
+    [SerializeField] RectTransform boardRoot;
+    [SerializeField] Card card;
+
+    private List<CardModel> models = new List<CardModel>();
+    private List<Card> cards = new List<Card>();
+
+    void Start()
+    {
+
+    }
 
 }
